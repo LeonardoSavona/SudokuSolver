b2e484cb1346cf2bb4ac43c8032254497e786237
LeonardoSavona <leonardosavona099@gmail.com>
2024-10-04 22:40:12 +0200

    solved master-3

--- src/main/java/com/example/sudoku/solver/Launcher.java
+++ src/main/java/com/example/sudoku/solver/Launcher.java
@@ -8,7 +8,7 @@ import java.io.File;
 
 public class Launcher {
 
-    private static final String LEVEL = "master-2";
+    private static final String LEVEL = "master-3";
 
     public static void main(String[] args) throws Exception {
         File sudokuFile = new File(Launcher.class.getClassLoader().getResource("sudoku/sudoku_"+LEVEL).toURI());
